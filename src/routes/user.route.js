e21704d15a368991ef56ea552620a7733b42c8a7
trandung2k1 <trandungitksnb2001@gmail.com>
2024-06-17 19:22:41 +0700

    update

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require('http-status-codes');
 const router = Router();
 
 router.get('/', (req, res) => {
-    const users = [];
+    const users = [{ id: 1, name: 'DungTran' }];
     return res.status(StatusCodes.OK).json(users);
 });
 module.exports = router;
