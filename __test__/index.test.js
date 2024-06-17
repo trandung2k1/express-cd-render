9797b796d836fc4cf3af750942f12b59d07b3403
trandung2k1 <trandungitksnb2001@gmail.com>
2024-06-17 19:31:04 +0700

    done

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../src');
 describe('GET /', () => {
-    it('Response with route /!', (done) => {
-        request(app).get('/').expect('Welcome to the server!', done);
+    it('Response with route welcome', (done) => {
+        request(app).get('/api/welcome').expect('Welcome to the server!', done);
     });
 });
